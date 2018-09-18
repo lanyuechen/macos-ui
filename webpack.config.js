c470d9694f668bb734a4b909c0ada3a171b4c097
zhenglipeng <1252012805@qq.com>
2018-09-18 17:51:56 +0800

    webpack配置修改

--- webpack.config.js
+++ webpack.config.js
@@ -75,13 +75,23 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
-          'file-loader'
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[ext]'
+            }
+          }
         ]
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
-          'file-loader'
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'fonts/[name].[ext]'
+            }
+          }
         ]
       }
     ]
